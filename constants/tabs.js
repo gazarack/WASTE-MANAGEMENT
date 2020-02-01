2e33f020698db01bb785a1874989739da631db48
Bestin Lalu <46861674+gazarack@users.noreply.github.com>
2020-02-02 01:56:07 +0530

    Add files via upload

---
+++ constants/tabs.js
@@ -0,0 +1,8 @@
+export default tabs = {
+  categories: [
+    { id: "popular", title: "Customer Support" },
+    { id: "beauty", title: "Cancel Request" },
+    { id: "fashion", title: "Request" },
+    { id: "car_motorcycle", title: "Profile" }
+  ]
+};
